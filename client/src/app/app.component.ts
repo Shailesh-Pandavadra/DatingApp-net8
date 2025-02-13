691b5d467461da4806b9597ea4c07ed1a9dbb276
Shailesh-Pandavadra <Pandavadrashailesh93@outlook.com>
2025-02-13 13:53:32 +0530

    End of section 6

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,33 +2,18 @@ import { Component, inject, OnInit } from '@angular/core';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
 import { HomeComponent } from "./home/home.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  // imports: [RouterOutlet
-  //   // ,NgFor
-  // ],
+  imports: [RouterOutlet, NavComponent
+    // , HomeComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [NavComponent, HomeComponent]
+  styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-
-  // Some of the angular functionality we're using did used to be 
-  // provided by the class, a TS class, or a JavaScript Class.
-  // But Angular seems to be using in toward using 
-  // functional components for certain features. 
-  // Possibly in future that's how they're going to deal with 
-  // an angular component as well
-
-  // constructor(private httpClient: HttpClient) {}
-
-  // alternate way to inject  into components & do not rely 
-  // on a constructor,
-  // because Constructor is something that we only get with a 
-  // JS or TS, we don't get it with normal JS function.
-
   private accountServie = inject(AccountService);
   title = 'DatingApp';
 
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, inject, input, output, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -14,7 +15,8 @@ export class RegisterComponent {
   // @Input() usersFromHomeComponent: any;
   // usersFromHomeComponent = input.required<any>();
   // @Output() cancelRegister = new EventEmitter();
-  cancelRegister = output<boolean>()
+  private toastr = inject(ToastrService);
+  cancelRegister = output<boolean>();
   model: any = {};
 
   register() {
@@ -24,9 +26,9 @@ export class RegisterComponent {
         console.log(response);
         this.cancel();
       },
-      error: error => {
-        console.log(error);
-      },
+      error: error =>
+        // console.log(error);
+        this.toastr.error(error.error)
     });
   }
 
